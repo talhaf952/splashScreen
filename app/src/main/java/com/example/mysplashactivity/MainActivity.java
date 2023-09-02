66ca88a33984df162619bcf13f381846946ecfd6
Dell <talhaf952@gmail.com>
2023-09-02 10:06:15 -0800

    MainActivity

---
+++ app/src/main/java/com/example/mysplashactivity/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mysplashactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish(); // Close the splash activity
+            }
+        }, SPLASH_DURATION);
+    }
+}
